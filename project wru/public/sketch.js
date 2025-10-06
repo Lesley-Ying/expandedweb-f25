71f2d908c5ba024376d8a0bd321e3b0660f714b2
Lesley Ying <sy4405@nyu.edu>
2025-10-06 14:26:29 +0800

    change project

--- project wru/public/sketch.js
+++ project wru/public/sketch.js
@@ -1,49 +1,113 @@
+let socket;
 let x;
 let y;
-let bgIsblack=true;
-let size=100;
-let changeSize=50;
-//sound trigger:
-//color change
-//if you're close to each other
-//how can you see yourself in others' eyes:if close, show stroke?
+let bgIsblack = true;
+let size = 100;
+let changeSize = 50;
+let transition = 0; 
+let winSound;
+let otherUsers = {};
+
+function preload(){
+  winSound=loadSound("win.wav")
+}
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  x=width/2;
-  y=height/2;
-  bgcolor=color(0);
-  holeColor=color(255);
+  x = width / 2;
+  y = height / 2;
+  bgcolor = color(0);
+  holeColor = color(255);
+  socket = io();
+  socket.on('State', function(data) {
+    console.log('Received state:', data);
+    bgIsBlack = data.bgIsBlack;
+    size = data.size;
+    changeSize = data.changeSize;
+    x = data.targetX * width;
+    y = data.targetY * height;
+    transition = 0;
+  });
+  socket.on('otherUser', function(data) {
+    otherUsers[data.id] = {
+      x: data.x * width,
+      y: data.y * height
+    };
+  });
+  socket.on('playSound', function() {
+    console.log('Playing win sound!');
+    winSound.play();
+  });
+
+  socket.on('userLeft', function(data) {
+    delete otherUsers[data];
+  });
 }
 
 function draw() {
-  background(bgcolor);
-  if(bgIsblack){
-    bgcolor=color(0);
-    holeColor=color(255);
-  }else{
-    bgcolor=color(255);
-    holeColor=color(0);
-  }
-  noCursor();
+  let c1 = color(0);
+  let c2 = color(255);
+  let progress = constrain(transition, 0, 1);
   
-   let distance=dist(mouseX,mouseY,x,y);
-  if(distance<1){
-    bgIsblack=!bgIsblack;
-   size+=changeSize;
-    if(size>=450){
-      changeSize*=-1;
-    }else if(size<=100){
-changeSize*=-1;
-    }
-    
-    console.log(size)
-    x=random(0,width);
-    y=random(0,height);
+  if (bgIsblack) {
+    bgcolor = lerpColor(c2, c1, progress);
+    holeColor = lerpColor(c1, c2, progress);
+  } else {
+    bgcolor = lerpColor(c1, c2, progress);
+    holeColor = lerpColor(c2, c1, progress);
   }
+
+  background(bgcolor);
+  noCursor();
+
+  let distance = dist(mouseX, mouseY, x, y);
+
+  if (distance < 1) {
+    // winSound.play();
+    // bgIsblack = !bgIsblack;
+    // transition = 0; 
+
+    // size += changeSize;
+    // if (size >= 450 || size <= 100) {
+    //   changeSize *= -1;
+    // }
+
+    // x = random(0, width);
+    // y = random(0, height);
+    let data={
+      id:socket.id
+    };
+    socket.emit('trigger',data)
+  }
+
+  //lerpColor
+  transition += 0.05;
+
+  //draw static circle
   noStroke();
   fill(holeColor);
-  circle(x,y,size);
+  circle(x, y, size);
+
+  //yourself
   fill(bgcolor);
-  circle(mouseX,mouseY,size);
-  
+  circle(mouseX, mouseY, size);
+  //others
+  for(let id in otherUsers){
+    let user=otherUsers[id];
+    fill(bgcolor);
+    circle(user.s,user.y,size)
+  }
+}
+function mouseMoved(){
+  let data={
+    x:mouseX/width,
+    y:mouseY/height,
+    id: socket.id,
+  }
+  socket.emit('mouseMove', data);
+}
+function mousePressed() {
+  userStartAudio();
+}
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
--- project wru/server.js
+++ project wru/server.js
@@ -11,7 +11,13 @@ let app = express();
 let server = http.createServer(app);
 let io = new socketIo.Server(server);
 let port = 3000;
-let strokes=[];
+let State = {
+  targetX: 0.5,  
+  targetY: 0.5,
+  bgIsBlack: true,
+  size: 100,
+  changeSize: 50
+};
 
 app.use(express.static('public'));
 
@@ -19,22 +25,39 @@ app.use(express.static('public'));
 
 io.on('connection', function(socket) {
   console.log(socket.id + ' connected from ' + socket.handshake.address);
-  socket.emit('allStrokes', strokes);
+  socket.emit('State', State);
+  socket.on('mouseMove',function(data){
 
-  // socket.on('disconnect', function(reason) {
-  //   console.log(socket.id + ' disconnected');
-  // });
-  socket.on('draw', function(data) {
-    socket.broadcast.emit('draw', data);
-    strokes.push(data);
+    socket.broadcast.emit('otherUser', {
+      id: socket.id,
+      x: data.x,
+      y: data.y
+    });
+  });
+  socket.on('trigger', function(data) {
+    console.log(socket.id + ' triggered!');
+    
+    State.bgIsBlack = !State.bgIsBlack;
+    State.size += State.changeSize;
+    
+  
+    if (State.size >= 450 || State.size <= 100) {
+      State.changeSize *= -1;
+    }
+    
+   
+    gameState.targetX = Math.random();
+    gameState.targetY = Math.random();
+    
+    io.emit('State', State);
+    
+    io.emit('playSound');
   });
 
-  // socket.onAny(function(event, ...args) {
-  //   console.log(socket.id, event, ...args);
-  //   socket.broadcast.emit(event, ...args);
-  // });
+
   socket.on('disconnect', function(reason) {
     console.log(socket.id + ' disconnected');
+    socket.broadcast.emit('userLeft', socket.id);
   });
   
 });
