2d99c2a8b7f3b3f774c030acc5dd54eb6f848a4d
Lesley Ying <sy4405@nyu.edu>
2025-10-06 12:25:59 +0800

    create project

---
+++ project wru/public/sketch.js
@@ -0,0 +1,50 @@
+// Expanded Web
+// NYUSH F25 - gohai
+
+let socket;
+
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+  background(204);
+  socket = io();
+
+  // socket.on('something', function(data) {
+  //   console.log('received', data);
+  //   fill(0);
+  //   ellipse(data.x, data.y, 10, 10);
+  //   // do something with the data
+  // });
+  socket.on('allStrokes', function(data) {
+    //background(255);
+    for (let i=0; i < data.length; i++) {
+      fill(0);
+      circle(data[i].x, data[i].y, 10);
+    }
+  });
+  socket.on('draw', function(data) {
+    console.log('Received draw from other user:', data);
+    fill(0);
+    ellipse(data.x, data.y, 10);
+  });
+}
+
+function draw() {
+  //background(204);
+}
+
+function mousePressed() {
+  let data = {
+    x: mouseX,
+    y: mouseY,
+    id: socket.id,  // often useful to have this id
+    // add other data to send here
+  }
+  fill(0);
+  ellipse(mouseX,mouseY,10,10);
+  socket.emit('draw', data);
+
+
+  // besides just "something" you can also have multiple,
+  // different kinds of messages (each with their own event
+  // handler in setup)
+}
---
+++ project wru/server.js
@@ -0,0 +1,46 @@
+// Expanded Web
+// NYUSH F25 - gohai
+
+// you probably don't even need to modify this file :)
+
+let express = require('express');
+let http = require('node:http');
+let socketIo = require('socket.io');
+
+let app = express();
+let server = http.createServer(app);
+let io = new socketIo.Server(server);
+let port = 3000;
+let strokes=[];
+
+app.use(express.static('public'));
+
+
+
+io.on('connection', function(socket) {
+  console.log(socket.id + ' connected from ' + socket.handshake.address);
+  socket.emit('allStrokes', strokes);
+
+  // socket.on('disconnect', function(reason) {
+  //   console.log(socket.id + ' disconnected');
+  // });
+  socket.on('draw', function(data) {
+    socket.broadcast.emit('draw', data);
+    strokes.push(data);
+  });
+
+  // socket.onAny(function(event, ...args) {
+  //   console.log(socket.id, event, ...args);
+  //   socket.broadcast.emit(event, ...args);
+  // });
+  socket.on('disconnect', function(reason) {
+    console.log(socket.id + ' disconnected');
+  });
+  
+});
+
+
+
+server.listen(port, "0.0.0.0", function() {
+  console.log('Example app listening on port ' + port);
+});
